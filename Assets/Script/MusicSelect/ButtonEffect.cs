8d59967a596b02ef480e3312bd403d939236c67d
しゅう <44467870+shumm7@users.noreply.github.com>
2019-08-27 04:14:53 +0900

    曲選択画面のUIを組んだ

---
+++ Assets/Script/MusicSelect/ButtonEffect.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEffect : MonoBehaviour
+{
+    [SerializeField] GameObject[] Button;
+    [SerializeField] AudioSource audioSource;
+
+    private GameObject[] ButtonOverray;
+
+    void Awake()
+    {
+        ButtonOverray = new GameObject[5];
+        for (int i = 0; i < 5; i++)
+        {
+            ButtonOverray[i] = Button[i].transform.Find("Button Pressed").gameObject;
+            ButtonOverray[i].SetActive(false);
+        }
+
+        
+    }
+
+    void Update()
+    {
+        //Sound
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+
+        //Effect
+        if (Input.GetKey(KeyCode.Q))
+        {
+            ButtonOverray[0].SetActive(true);
+        }
+        else
+        {
+            ButtonOverray[0].SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            ButtonOverray[1].SetActive(true);
+        }
+        else
+        {
+            ButtonOverray[1].SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            ButtonOverray[2].SetActive(true);
+        }
+        else
+        {
+            ButtonOverray[2].SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            ButtonOverray[3].SetActive(true);
+        }
+        else
+        {
+            ButtonOverray[3].SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            ButtonOverray[4].SetActive(true);
+        }
+        else
+        {
+            ButtonOverray[4].SetActive(false);
+        }
+    }
+}
